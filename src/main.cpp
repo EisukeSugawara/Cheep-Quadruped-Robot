687cee92a41a0888b78c9b48f0bb18532f754124
Eisuke_Sugawara <c640268@st.kanazawa-it.ac.jp>
2025-04-27 00:26:05 +0900

    feat: Adding inverse kinematics and coordinate transforms for single leg

--- src/main.cpp
+++ src/main.cpp
@@ -1,97 +1,88 @@
 #include <Arduino.h>
+#include <ArduinoEigenDense.h>
 
-// 時間変数を定義
-unsigned long time1 = 0;
-unsigned long time2 = 0;
-unsigned long time3 = 0;
+using namespace Eigen;
 
 // 各リンクの長さを定義
-const float l_coxa = 30.0;
-const float l_femur = 60.0;
-const float l_tibia = 80.0;
+const float length_coxa = 30.0;
+const float length_femur = 60.0;
+const float length_tibia = 80.0;
 
-const float body_width = 100.0; // ボディの幅
-const float body_length = 100.0; // ボディの長さ
-
-// ボディ座標系の初期位置を定義
-const float x_origin_body = 50.0;
-const float y_origin_body = 140.0;
-const float z_origin_body = -80.0;
-
-// 定数：ラジアンから度への変換
-const float RAD2DEG = 180.0 / PI;
-
-// ラジアンを度に変換するヘルパー関数
-inline float rad2deg(float rad) {
-  return rad * RAD2DEG;
-}
-
-// 同次変換後の座標をまとめる構造体
-struct Coordinates {
+// 足先座標を格納する構造体
+struct FootPos {
   float x;
   float y;
   float z;
 };
 
-// 同次変換の関数
-Coordinates homogeneousTransformation(float x, float y, float z) {
-  Coordinates transformed;
-  transformed.x = y - body_length / 2; // rotateMatrix の第1行と translationVector の x 要素
-  transformed.y = -x + body_width / 2; // rotateMatrix の第2行と translationVector の y 要素
-  transformed.z = z;                   // rotateMatrix の第3行と translationVector の z 要素
-  return transformed;
+// 座標変換を行う関数
+FootPos coordTf(float x, float y, float z) {
+
+  Matrix3f rotateMatrix;
+  rotateMatrix << 0, 1, 0,
+                  -1, 0,0,
+                  0, 0, 1;
+
+  Vector3f vectorBody;
+  vectorBody << x,
+                y,
+                z;
+
+  Vector3f translationVector;
+  translationVector << -50,
+                        50,
+                        0;
+
+  Vector3f result = rotateMatrix * vectorBody + translationVector;
+
+  FootPos foot_pos_leg = {result(0), result(1), result(2)};
+
+  return foot_pos_leg;
 }
 
-// 逆運動学の結果をまとめる構造体
-struct JointAngles {
-  float theta_coxa;
-  float theta_femur;
-  float theta_tibia;
+// 関節角度を格納する構造体
+struct JointAngle {
+  float coxa;
+  float femur;
+  float tibia;
 };
 
-// 逆運動学の計算関数
-JointAngles inverseKinematics(float x, float y, float z) {
-  JointAngles angle;
+// 逆運動学を行う関数
+JointAngle IK(FootPos foot_pos_leg) {
 
-  float r = sqrt(x * x + y * y);
-  float s = sqrt((r - l_coxa) * (r - l_coxa) + z * z);
-  
-  float theta_ABC = acos((l_femur * l_femur + l_tibia * l_tibia - s * s) / (2 * l_femur * l_tibia));
-  float theta_alpha = atan2(-z, r - l_coxa);
-  float theta_BAC = acos((l_femur * l_femur + s * s - l_tibia * l_tibia) / (2 * l_femur * s));
+  JointAngle angle;
 
-  angle.theta_coxa  = rad2deg(atan2(y, x)) + 45.0;
-  angle.theta_femur = rad2deg(theta_alpha - theta_BAC);
-  angle.theta_tibia = rad2deg(PI - theta_ABC) - 90.0; 
-  
+  float r = sqrt(foot_pos_leg.x*foot_pos_leg.x + foot_pos_leg.y*foot_pos_leg.y);
+  float s = sqrt((r - length_coxa)*(r - length_coxa) + foot_pos_leg.z*foot_pos_leg.z);
+  float theta_ABC = acos((length_femur*length_femur + length_tibia*length_tibia - s*s) / (2*length_femur*length_tibia));
+  float theta_alpha = atan2(-foot_pos_leg.z, r - length_coxa);
+  float theta_BAC = acos((length_femur*length_femur + s*s - length_tibia*length_tibia) / (2*length_femur*s));
+
+  angle.coxa = atan2(foot_pos_leg.y, foot_pos_leg.x) * 180 / PI;  // theta_coxa
+  angle.femur = (theta_alpha - theta_BAC) * 180.0 / PI;           // theta_femur
+  angle.tibia = (PI - theta_ABC) * 180.0 / PI;                    // theta_tibia
+
   return angle;
-}
-
+};
 
 void setup(){
   Serial.begin(115200);
 
-  delay(3000);
+  delay(5000);
 
-  time1 = micros();
-
-  Coordinates transformed = homogeneousTransformation(x_origin_body, y_origin_body, z_origin_body);
-
-  time2 = micros();
-
-  JointAngles angle = inverseKinematics(transformed.x, transformed.y, transformed.z);
+  FootPos foot_pos_leg = coordTf(50, 140, -80);
   
-  time3 = micros();
+  Serial.printf("foot_pos_leg.x: %f\n", foot_pos_leg.x);
+  Serial.printf("foot_pos_leg.y: %f\n", foot_pos_leg.y);
+  Serial.printf("foot_pos_leg.z: %f\n", foot_pos_leg.z);
+  Serial.printf("\n");
 
-  Serial.printf("x: %f, y: %f, z: %f\n", transformed.x, transformed.y, transformed.z);
-  Serial.printf("theta_coxa: %f\n", angle.theta_coxa);
-  Serial.printf("theta_femur: %f\n", angle.theta_femur);
-  Serial.printf("theta_tibia: %f\n", angle.theta_tibia);
-  Serial.print("\n");  
+  JointAngle angle_0 = IK(foot_pos_leg);
 
-  Serial.printf("HT_time: %d us\n", time2- time1);
-  Serial.printf("IK_time: %d us\n", time3 - time2);
-  Serial.print("\n");
+  Serial.printf("leg_0.coxa: %f\n", angle_0.coxa);
+  Serial.printf("leg_0.femur: %f\n", angle_0.femur);
+  Serial.printf("leg_0.tibia: %f\n", angle_0.tibia);
+
 }
 
 void loop() {}
